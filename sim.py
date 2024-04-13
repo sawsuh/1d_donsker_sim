c1d74085aed10814707ac63d72c829840edc7d17
Prashant Rao <prao060@gmail.com>
2024-04-14 05:47:54 +1000

    switched to functools cache

--- sim.py
+++ sim.py
@@ -2,6 +2,7 @@ from typing import Callable, Tuple, Dict, Generic, TypeVar
 from dataclasses import dataclass
 from enum import Enum
 import math
+from functools import cache, wraps
 
 
 class PlusMinus(Enum):
@@ -33,19 +34,6 @@ I = TypeVar("I")
 O = TypeVar("O")
 
 
-class Memoized(Generic[I, O]):
-    def __init__(self, function: Callable[[I], O]) -> None:
-        self.function = function
-        self.cache: Dict[I, O] = {}
-
-    def eval(self, arg: I) -> O:
-        try:
-            return self.cache[arg]
-        except KeyError:
-            self.cache[arg] = self.function(arg)
-            return self.cache[arg]
-
-
 def integrate(
     function: Callable[[float], float], left: float, right: float, increment=0.001
 ) -> float:
@@ -67,13 +55,18 @@ def compute_psi(operator: Operator, x0: float) -> Callable[[float], float]:
 
 
 def compute_v0(
-    left: float, right: float, operator: Operator, pm: PlusMinus, psi: None | Memoized
+    left: float,
+    right: float,
+    operator: Operator,
+    pm: PlusMinus,
+    psi: None | Callable[[float], float],
 ) -> Callable[[float], float]:
     if psi is None:
-        psi = Memoized(compute_psi(operator=operator, x0=left))
+        psi_helper = compute_psi(operator=operator, x0=left)
+        psi = wraps(psi_helper)(cache(psi_helper))
 
     def integrand(y: float) -> float:
-        return math.exp(psi.eval(y)) / operator.a(y)
+        return math.exp(psi(y)) / operator.a(y)
 
     def integrate_from_to(xy: Tuple[float, float]) -> float:
         return integrate(function=integrand, left=xy[0], right=xy[1])
@@ -90,16 +83,18 @@ def compute_v1(
     right: float,
     operator: Operator,
     pm: PlusMinus,
-    psi: None | Memoized,
-    v0: None | Memoized,
+    psi: None | Callable[[float], float],
+    v0: None | Callable[[float], float],
 ) -> Callable[[float], float]:
     # use 1 memoization for psi and 1 memoization for v0
     # initialise both here but allow for non memoized
     if psi is None:
-        psi = Memoized(compute_psi(operator=operator, x0=left))
+        psi_helper = compute_psi(operator=operator, x0=left)
+        psi = wraps(psi_helper)(cache(psi_helper))
 
     if v0 is None:
-        v0 = Memoized(
-            compute_v0(left=left, right=right, operator=operator, pm=pm, psi=psi)
+        v0_helper = compute_v0(
+            left=left, right=right, operator=operator, pm=pm, psi=psi
         )
+        v0 = wraps(v0_helper)(cache(v0_helper))
     return lambda _: 0
