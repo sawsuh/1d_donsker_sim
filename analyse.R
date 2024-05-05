91bee80870fb357c561ed873f65a3b34f833754d
sawsuh <prao060@gmail.com>
2024-05-06 00:05:25 +1000

    added const keywords and converted main loop to reduction

--- analyse.R
+++ analyse.R
@@ -2,19 +2,17 @@ library(ggplot2)
 vals <- read.csv('res.csv', header=F)
 x <- vals$V1
 
-binwidth <- 0.01*4
+binwidth <- 0.01*2
 args <- commandArgs(trailingOnly=T)
 if (length(args) > 1) {
 	if (args[2] %in% c("_FIG1", "_FIG2", "_FIG3")) {
-		binwidth <- 0.02*4
+		binwidth <- 0.02*2
 	}
 	else if (args[2] %in% c("_FIG4", "_FIG5")) {
-		binwidth <- 0.05*4
+		binwidth <- 0.05*2
 	}
 }
 
 ggplot(vals, aes(x=V1, y=after_stat(density))) +
 	geom_histogram(binwidth=binwidth) +
-	xlab("X_T")
-# +
-	# ylim(0,1.4)
+	xlab("X_t")
--- sim.cpp
+++ sim.cpp
@@ -5,6 +5,11 @@
 #include <memory>
 #include <random>
 
+#pragma omp declare reduction(                                                 \
+        vec_concat : std::vector<long double> : omp_out.insert(                \
+                omp_out.end(), omp_in.begin(), omp_in.end()))                  \
+    initializer(omp_priv = omp_orig)
+
 // represents left or right exit
 enum PlusMinus { plus, minus };
 // cell: contains values of left and right neighbours
@@ -18,6 +23,7 @@ struct cell {
 const int ROUNDS = 10000;
 // Interval to use for numerical integration
 const long double INTEGRATION_INC = 0.00001;
+// default: Brownian motion
 #if !(defined(_FIG1) || defined(_FIG2) || defined(_FIG3) || defined(_FIG4) ||  \
       defined(_FIG5))
 // Starting point in grid
@@ -25,22 +31,23 @@ const long double START = 0;
 // Time at which we want to sample X_t
 const long double TIME = 1;
 // a
-long double a(long double x) { return 1; }
+const long double a(long double x) { return 1; }
 // rho
-long double rho(long double x) { return 1; }
+const long double rho(long double x) { return 1; }
 // b
-long double b(long double x) { return 0; }
+const long double b(long double x) { return 0; }
 // GRID SPECIFICATION
 // args: point in grid
 // returns: two adjacent points
-cell get_adjacent(long double point) {
+const cell get_adjacent(long double point) {
   return cell{point - 0.01, point + 0.01};
 }
 #endif
 
+// defining params for each of the figs
 #if (defined(_FIG1) || defined(_FIG2) || defined(_FIG3))
 const long double START = 1;
-long double a(long double x) {
+const long double a(long double x) {
   if (x < -1.5) {
     return 1;
   } else if (x < 0.5) {
@@ -49,7 +56,7 @@ long double a(long double x) {
     return 1;
   }
 }
-long double rho(long double x) {
+const long double rho(long double x) {
   if (x < -0.5) {
     return 1;
   } else if (x < 0.5) {
@@ -58,8 +65,8 @@ long double rho(long double x) {
     return 1;
   }
 }
-long double b(long double x) { return 0; }
-cell get_adjacent(long double point) {
+const long double b(long double x) { return 0; }
+const cell get_adjacent(long double point) {
   return cell{point - 0.02, point + 0.02};
 }
 #endif
@@ -73,18 +80,18 @@ const long double TIME = 1.5;
 #if (defined(_FIG4) || defined(_FIG5))
 const long double START = 0;
 const long double TIME = 1;
-long double b(long double x) { return 0; }
-cell get_adjacent(long double point) {
+const long double b(long double x) { return 0; }
+const cell get_adjacent(long double point) {
   return cell{point - 0.05, point + 0.05};
 }
 #endif
 
 #if defined(_FIG4)
-long double a(long double x) { return (x < 0) ? 1 : 5; }
-long double rho(long double x) { return 1; }
+const long double a(long double x) { return (x < 0) ? 1 : 5; }
+const long double rho(long double x) { return 1; }
 #elif defined(_FIG5)
-long double a(long double x) { return 1; }
-long double rho(long double x) { return (x < 0) ? 1 : 5; }
+const long double a(long double x) { return 1; }
+const long double rho(long double x) { return (x < 0) ? 1 : 5; }
 #endif
 
 // =============================
@@ -93,10 +100,10 @@ long double rho(long double x) { return (x < 0) ? 1 : 5; }
 // contains left and right exit probabilities and
 //  left and right exit times conditional on left and right exit probabilities
 struct cellData {
-  long double time_left;
-  long double time_right;
-  long double prob_left;
-  long double prob_right;
+  const long double time_left;
+  const long double time_right;
+  const long double prob_left;
+  const long double prob_right;
 };
 
 // vector that we use to cache data for our integral
@@ -111,13 +118,9 @@ template <typename DT> void push_safe(std::vector<DT> &vec, int idx, DT x) {
 class CellDataCalculator {
 public:
   // boundary and start point
-  long double left, right, x;
-  CellDataCalculator(long double l, long double r, long double y) {
-    left = l;
-    right = r;
-    x = y;
-    integration_inc = INTEGRATION_INC;
-  }
+  const long double left, right, x;
+  CellDataCalculator(long double l, long double r, long double y)
+      : left(l), right(r), x(y), integration_inc(INTEGRATION_INC) {}
   // calculate data (exit times and probabilities)
   cellData compute_cell_data() {
     // precompute table of psi values
@@ -125,11 +128,11 @@ public:
     // precompute table of s values
     gen_s_table();
     // we can now compute exit probabilities
-    long double prob_left = v0minus(x);
-    long double prob_right = v0plus(x);
+    const long double prob_left = v0minus(x);
+    const long double prob_right = v0plus(x);
     // we can now compute exit times
-    long double time_left_ind = v1minus(x);
-    long double time_right_ind = v1plus(x);
+    const long double time_left_ind = v1minus(x);
+    const long double time_right_ind = v1plus(x);
     return cellData{time_left_ind / prob_left, time_right_ind / prob_right,
                     prob_left, prob_right};
   }
@@ -138,11 +141,13 @@ private:
   // tables of values for psi and s
   std::vector<long double> psi_values, s_values;
   // increment used for numerical integration
-  long double integration_inc;
+  const long double integration_inc;
   // arg: point
   // returns: index in table corresponding to the point (starts at 0)
   // (for table access)
-  int get_index(long double x) { return round((x - left) / integration_inc); }
+  const int get_index(long double x) {
+    return round((x - left) / integration_inc);
+  }
   // progressively precompute values of psi in one pass
   void gen_psi_table() {
     // reserve vector (we know the necessary length)
@@ -165,7 +170,7 @@ private:
     }
   }
   // all the necessary values should be stored in cache, throw an error if not
-  long double psi(long double x) { return psi_values.at(get_index(x)); }
+  const long double psi(long double x) { return psi_values.at(get_index(x)); }
   // precompute values of s, same strategy as psi
   // (do integral in one pass, storing each step as the integral to that point)
   void gen_s_table() {
@@ -188,13 +193,13 @@ private:
     }
   }
   // all values should be precomputed, if not throw an error
-  long double s(long double x) { return s_values.at(get_index(x)); }
+  const long double s(long double x) { return s_values.at(get_index(x)); }
   // Now that we have s, we can compute v0plus
-  long double v0plus(long double x) { return s(x) / s(right); }
+  const long double v0plus(long double x) { return s(x) / s(right); }
   // v0minus = 1-v0plus
-  long double v0minus(long double x) { return 1 - v0plus(x); }
+  const long double v0minus(long double x) { return 1 - v0plus(x); }
   // We can also express G using s
-  long double G(long double x, long double y) {
+  const long double G(long double x, long double y) {
     if (x <= y) {
       return 2 * (s(x) - s(left)) * (s(right) - s(y)) / (s(right) - s(left));
     } else {
@@ -204,7 +209,7 @@ private:
   // We can use G together with psi and v0plus to compute v1plus
   // We only do this once so we don't need to cache
   // We use the trapezoid rule
-  long double v1plus(long double x) {
+  const long double v1plus(long double x) {
     long double integral = 0;
     for (int y_idx = 0; y_idx < get_index(right); y_idx++) {
       long double y = y_idx * integration_inc + left;
@@ -220,7 +225,7 @@ private:
   // We can use G together with psi and v0minus to compute v1minus
   // We only do this once so we don't need to cache
   // We use the trapezoid rule
-  long double v1minus(long double x) {
+  const long double v1minus(long double x) {
     long double integral = 0;
     for (int y_idx = 0; y_idx < get_index(right); y_idx++) {
       long double y = y_idx * integration_inc + left;
@@ -241,8 +246,8 @@ private:
 //  how long it takes us to get there
 //  we also track where our index in the grid changes
 struct increment {
-  long double next_point, delta_t;
-  int change_grid_idx;
+  const long double next_point, delta_t;
+  const int change_grid_idx;
 };
 // this is a structure for storing data we compute correponding to points in the
 // grid
@@ -251,9 +256,7 @@ public:
   // actually holds the data
   std::list<cellData> container;
   // initialise with data for the start point
-  cell_cache(long double start) {
-    left = 0;
-    right = 0;
+  cell_cache(long double start) : left(0), right(0) {
     // get neighbours
     cell lr = get_adjacent(start);
     // Compute values
@@ -264,28 +267,32 @@ public:
     it = container.begin();
   }
   // return iterator to start
-  std::list<cellData>::iterator get_iter() { return it; }
+  const std::list<cellData>::iterator get_iter() { return it; }
   // get left endpoint
-  int get_left() {
+  const int get_left() {
     int l;
+    // atomic for thread safety
 #pragma omp atomic read
     l = left;
     return l;
   }
   // get right endpoint
-  int get_right() {
+  const int get_right() {
     int r;
+    // atomic for thread safety
 #pragma omp atomic read
     r = right;
     return r;
   }
   // advance left endpoint
   void inc_left() {
+    // atomic for thread safety
 #pragma omp atomic update
     left--;
   }
   // advance right endpoint
   void inc_right() {
+    // atomic for thread safety
 #pragma omp atomic update
     right++;
   }
@@ -297,6 +304,7 @@ private:
   int left, right;
 };
 // object that each thread uses to walk the cache
+// effectively a wrapper around an iterator
 class cell_cache_walker {
 public:
   // uses a shared pointer to the cache
@@ -307,14 +315,15 @@ public:
   }
   // check if cache holds an item
   // left and right are the ends of the cache
-  bool contains(int goal) {
+  // these are already atomic so no race condition here
+  const bool contains(int goal) {
     return (cache->get_left() <= goal) && (goal <= cache->get_right());
   }
   // return an item that is 0 or 1 steps away
   // (and walk there)
   // assumes we already checked existence
-  cellData at(int goal) {
-    int change = goal - idx;
+  const cellData at(int goal) {
+    const int change = goal - idx;
     // increment internal position
     idx += change;
     // update iterator
@@ -328,19 +337,17 @@ public:
     throw std::out_of_range("indexing too far away");
   }
   // safely insert an item
-  // returns bool representing whether insertion
-  bool insert(int goal, cellData x) {
-    bool wrote = false;
-    int change = goal - idx;
+  void insert(int goal, cellData x) {
+    const int change = goal - idx;
     // left frontier
     if (change == -1) {
       // insert if it isn't there
+      // critical to prevent races
 #pragma omp critical(cache)
       {
         if (goal == cache->get_left() - 1) {
           cache->container.push_front(x);
           cache->inc_left();
-          wrote = true;
         }
       }
       // walk this instance
@@ -349,12 +356,12 @@ public:
     } else if (change == 1) {
       // right frontier
       // insert if not there
+      // critical to prevent races
 #pragma omp critical(cache)
       {
         if (goal == cache->get_right() + 1) {
           cache->container.push_back(x);
           cache->inc_right();
-          wrote = true;
         }
       }
       // walk this instance
@@ -363,12 +370,11 @@ public:
     } else {
       throw std::out_of_range("inserting not exactly 1 away");
     }
-    return wrote;
   }
 
 private:
   // stores actual cache
-  std::shared_ptr<cell_cache> cache;
+  const std::shared_ptr<cell_cache> cache;
   // iterator that we walk around
   std::list<cellData>::iterator it;
   // tracks where in the grid our iterator is
@@ -379,18 +385,17 @@ private:
 class Simulator {
 public:
   // Start point
-  long double start;
+  const long double start;
   // initialise cache
-  Simulator(long double x) : cache_ptr(std::make_shared<cell_cache>(x)) {
-    start = x;
-  }
-  // Holds results
-  std::vector<long double> results;
+  Simulator(long double x)
+      : start(x), cache_ptr(std::make_shared<cell_cache>(x)) {}
   // Runs simulation
   void simulate(long double t, int rounds = ROUNDS) {
-#pragma omp parallel for
+    // multithread this
+    std::vector<long double> results;
+#pragma omp parallel for reduction(vec_concat : results)
     for (int idx = 0; idx < rounds; idx++) {
-      run_sim(t, idx);
+      results.push_back(run_sim(t, idx));
     }
     // Write results to "res.csv"
     std::ofstream output_file("res.csv");
@@ -403,10 +408,10 @@ public:
 
 private:
   // Holds computed data for cells we have visited
-  std::shared_ptr<cell_cache> cache_ptr;
+  const std::shared_ptr<cell_cache> cache_ptr;
   // computes values
-  cellData get_data(cell_cache_walker &walker, long double point,
-                    int grid_idx) {
+  const cellData get_data(cell_cache_walker &walker, long double point,
+                          int grid_idx) {
     // If the point is in our cache
     // (we already visited it and have data)
     if (walker.contains(grid_idx)) {
@@ -416,10 +421,10 @@ private:
     // Compute the necessary data
 
     // get neighbours
-    cell lr = get_adjacent(point);
+    const cell lr = get_adjacent(point);
     // Compute values
     CellDataCalculator calc(lr.left, lr.right, point);
-    cellData out = calc.compute_cell_data();
+    const cellData out = calc.compute_cell_data();
     // write computed data to cache
     walker.insert(grid_idx, out);
 
@@ -427,12 +432,12 @@ private:
     return out;
   }
   // take a step from a point
-  increment next_point(cell_cache_walker &walker, long double point,
-                       std::mt19937 &rng, int grid_idx) {
+  const increment next_point(cell_cache_walker &walker, long double point,
+                             std::mt19937 &rng, int grid_idx) {
     // get neighbours
-    cell lr = get_adjacent(point);
+    const cell lr = get_adjacent(point);
     // get data
-    cellData point_data = get_data(walker, point, grid_idx);
+    const cellData point_data = get_data(walker, point, grid_idx);
     // simulate step
     std::bernoulli_distribution d(point_data.prob_right);
     if (d(rng)) {
@@ -446,7 +451,7 @@ private:
   // run 1 thread
   // takes start point
   // idx represents iteration number (for logging)
-  void run_sim(long double t, int idx) {
+  long double run_sim(long double t, int idx) {
 #ifdef _DEBUG
 #pragma omp critical(cout)
     std::cout << idx << " started\n";
@@ -476,9 +481,7 @@ private:
 #pragma omp critical(cout)
     std::cout << idx << " finished at " << cur << "\n";
 #endif
-// write result
-#pragma omp critical(results)
-    results.push_back(cur);
+    return cur;
   }
 };
 
